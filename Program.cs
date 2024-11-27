ef0b07c335d96e792b44651588e5c1073fe7a1ed
Placko135 <papplaszlolevente@gmail.com>
2024-11-27 11:37:54 +0100

    pushá

--- Program.cs
+++ Program.cs
@@ -23,8 +23,39 @@ class TorpedoGameServer
             int clientId = clientIdCounter++;
             clients.TryAdd(clientId, client);
             Console.WriteLine($"Client {clientId} connected.");
+
+            // Send the player ID to the client immediately upon connection
+            if (!await SendPlayerID(client, clientId))
+            {
+                Console.WriteLine($"Client {clientId} has been disconnected due to invalid player ID.");
+                client.Close();
+                continue; // Skip further processing for this client
+            }
+
+            // Start handling the client
             _ = HandleClient(client, clientId);
         }
+    }
+
+    private static async Task<bool> SendPlayerID(TcpClient client, int clientId)
+    {
+        string playerIdMessage;
+
+        // Check the value of clientId and respond accordingly
+        if (clientId > 2)
+        {
+            playerIdMessage = "PlayerID:-1"; // Send -1 to indicate an invalid player ID
+        }
+        else
+        {
+            playerIdMessage = $"PlayerID:{clientId}"; // Format the message to indicate it's a valid player ID
+        }
+
+        byte[] responseData = Encoding.UTF8.GetBytes(playerIdMessage);
+        NetworkStream stream = client.GetStream();
+        await stream.WriteAsync(responseData, 0, responseData.Length);
+
+        return clientId <= 2; // Return true if valid, false if invalid
     }
 
     private static async Task HandleClient(TcpClient client, int clientId)
@@ -54,9 +85,7 @@ class TorpedoGameServer
     private static string ProcessMessage(string message, int clientId)
     {
         // Here you can implement logic to process the message
-        // For example, if the message is a hit on a cell, you can log it or update game state
         // For now, we will just echo the message back with the client ID
-
         return $"Client {clientId} says: {message}";
     }
 
